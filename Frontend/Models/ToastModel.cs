2c778cc91d441d973cbc3fd1c50b00627b2b6f9f
Benoit Podwinski <me@benoitpodwinski.com>
2025-01-15 17:54:47 +0100

    Rework Toasts

---
+++ Frontend/Models/ToastModel.cs
@@ -0,0 +1,18 @@
+﻿namespace Frontend.Models
+{
+    public class ToastModel
+    {
+        public string? Id { get; set; }
+        public string? Title { get; set; }
+        public string? Message { get; set; }
+        public ToastSeverity Severity { get; set; } = ToastSeverity.Info;
+    }
+
+    public enum ToastSeverity
+    {
+        Info,
+        Success,
+        Warning,
+        Error
+    }
+}
